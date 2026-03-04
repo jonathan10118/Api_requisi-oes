758bf597b84f8c4c08063fd8ce26cb603ca395e5
c4stroleo <leonardo.castro.oliveira@escola.pr.gov.br>
2026-03-04 10:56:58 -0300

    feat:incluido endpont alistamento

---
+++ README.md
@@ -0,0 +1,11 @@
+projetp de api de requisicoes
+
+calcula o imc da pessoa
+recebe nome,idade,altura e peso
+retorna o imc e a situacao
+
+calcula a media de um aluno
+recebe nome, nota 1 e nota 2
+retorna o nome e a media e se o aluno foi aprovado ou reprovado
+
+retorna uma saudacao
--- index.js
+++ index.js
@@ -21,20 +21,77 @@ app.post("/imc", (req, res) => {
   const { nome, idade ,altura, peso } = req.body;
   
   if(!nome || !idade || !altura || !peso){
-    return res.status(400).json(
-      { 
+    return res.status(400).json({ 
         erro: "Dados incompletos"
-      });
+    });
   }
+
   const imc = peso / (altura * altura);
-  res.json
-  ({ 
+
+  res.json({ 
     nome,
     imc: imc.toFixed(2)
   });
-
 });
 
+
+// 🔹 Nova rota - Média do aluno
+app.post("/media", (req, res) => {
+  const { nome, nota1, nota2 } = req.body;
+
+  if (!nome || nota1 === undefined || nota2 === undefined) {
+    return res.status(400).json({
+      erro: "Dados incompletos"
+    });
+  }
+
+  const media = (nota1 + nota2) / 2;
+  const status = media >= 7 ? "Aprovado" : "Reprovado";
+
+  res.json({
+    nome,
+    media: media.toFixed(2),
+    status
+  });
+}); 
+// 🔹 Nova Rota - Alistamento
+app.post("/alistamento", (req, res) => {
+  const { nome, idade, sexo } = req.body;
+
+  if (!nome || !idade || !sexo) {
+    return res.status(400).json({
+      erro: "Dados incompletos"
+    });
+  }
+
+  if (sexo.toLowerCase() === "masculino") {
+
+    if (idade < 18) {
+      res.json({
+        nome,
+        mensagem: "Ainda vai se alistar aos 18 anos."
+      });
+
+    } else if (idade === 18) {
+      res.json({
+        nome,
+        mensagem: "Deve se alistar este ano!"
+      });
+
+    } else {
+      res.json({
+        nome,
+        mensagem: "Já passou da idade de alistamento obrigatório."
+      });
+    }
+
+  } else {
+    res.json({
+      nome,
+      mensagem: "Alistamento militar não é obrigatório para este sexo."
+    });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
